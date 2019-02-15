c5b891adecbfe5c6bbe7cfd7b1bb896edfe8a2a1
Lori <Lori@loris-macbook-air.local>
2019-02-15 11:23:40 +0100

    Fehler Zugriffsrechte

--- Beatticks/ViewController.swift
+++ Beatticks/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        func applicationShouldRequestHealthAuthorization(application: UIApplication) {
+        func applicationShouldRequestHealthAuthorization() {
             healthStore.handleAuthorizationForExtension { (success, error) -> Void in
                 //...
             }
@@ -31,47 +31,53 @@ class ViewController: UIViewController {
             // Provide summary information when creating the workout.
             let run = HKWorkout(activityType: HKWorkoutActivityType.running, start: start, end: end)
             
-            // Save the workout before adding detailed samples.
-            self.healthStore.save(run) { (success, error) -> Void in
-                guard success else {
-                    // Perform proper error handling here...
-                    fatalError("*** An error occurred while saving the " +
-                        "workout: \(error?.localizedDescription ?? "")")
-                }
-            }
-            // Add optional, detailed information for each time interval
-            var samples: [HKQuantitySample] = []
-            
             guard let heartRateType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
                 fatalError("*** Unable to create a heart rate type ***")
             }
             
-            let heartRateForInterval = HKQuantity(unit: HKUnit(from: "count/min"),
-                                                  doubleValue: 95.0)
-            
-            let heartRateForIntervalSample =
-                HKQuantitySample(type: heartRateType, quantity: heartRateForInterval,
-                                 start: start, end: end)
-            
-            samples.append(heartRateForIntervalSample)
-            
-            // Continue adding detailed samples...
-            
-            // Add all the samples to the workout.
-            self.healthStore.add(samples, to: run) { (success, error) -> Void in
-                guard success else {
-                    // Perform proper error handling here...
-                    fatalError("*** An error occurred while adding a " +
-                        "sample to the workout: \(error?.localizedDescription)")
+            healthStore.requestAuthorization(toShare: [heartRateType], read:[heartRateType], completion:{(success, error) in
+                // Exception-code
+                
+                // Save the workout before adding detailed samples.
+                self.healthStore.save(run) { (success, error) -> Void in
+                    guard success else {
+                        // Perform proper error handling here...
+                        fatalError("*** An error occurred while saving the " +
+                            "workout: \(error?.localizedDescription ?? "")")
+                    }
                 }
-            }
+                // Add optional, detailed information for each time interval
+                var samples: [HKQuantitySample] = []
+                
+                let heartRateForInterval = HKQuantity(unit: HKUnit(from: "count/min"),
+                                                      doubleValue: 95.0)
+                
+                let heartRateForIntervalSample =
+                    HKQuantitySample(type: heartRateType, quantity: heartRateForInterval,
+                                     start: start, end: end)
+                
+                samples.append(heartRateForIntervalSample)
+                
+                // Continue adding detailed samples...
+                
+                // Add all the samples to the workout.
+                self.healthStore.add(samples, to: run) { (success, error) -> Void in
+                    guard success else {
+                        // Perform proper error handling here...
+                        fatalError("*** An error occurred while adding a " +
+                            "sample to the workout: \(error?.localizedDescription ?? "")")
+                    }
+                }
+            })
         }
-        //applicationShouldRequestHealthAuthorization(application: )
+        
+        applicationShouldRequestHealthAuthorization()
+        
         let heartRateType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
         
         if (HKHealthStore.isHealthDataAvailable()){
             var csvString = "Time,Date,Heartrate(BPM)\n"
-            //healthStore.requestAuthorization(toShare: nil, read:[heartRateType], completion:{(success, error) in
+            
             let sortByTime = NSSortDescriptor(key:HKSampleSortIdentifierEndDate, ascending:false)
             let timeFormatter = DateFormatter()
             timeFormatter.dateFormat = "hh:mm:ss"
@@ -100,7 +106,7 @@ class ViewController: UIViewController {
                 
             })
             self.healthStore.execute(query)
-            //})
+            
         }
     }
 }
